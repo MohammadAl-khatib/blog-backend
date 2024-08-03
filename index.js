09fdeb1bb1211ecc9d039e0746dc45ee357f6a9a
MohammadAl-khatib <alk20489@gmail.com>
2024-08-03 15:51:44 +0300

    eslint

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-app.get("/", (req, res) => {
-  res.json("server started successfully");
-});
+app.get('/', (req, res) => {
+  res.json('server started successfully')
+})
 
-app.listen(4000, "localhost", () => {
-  console.log("server started successfully");
-});
+app.listen(4000, 'localhost', () => {
+  console.log('server started successfully')
+})
