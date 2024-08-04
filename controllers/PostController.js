34a1244d0f713b872fd3aee258787e8205f4458c
MohammadAl-khatib <alk20489@gmail.com>
2024-08-04 20:25:54 +0300

    complete create post

---
+++ controllers/PostController.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const Post = require('../models/Post.js');
+
+const secret = process.env.jwtSecret;
+
+async function createPostController (req, res) {
+  try {
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (error, info) => {
+      if (error) {
+        console.error('Error verifying token:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+      const { title, summary, content } = req.body;
+      const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover: newPath,
+        author: info.id
+      });
+      res.json(postDoc);
+    });
+  } catch (error) {
+    console.error('Error in createPostController', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+module.exports = createPostController;
--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const multer = require('multer'); // for loading files
+
 require('dotenv').config();
 
-const registerController = require('./controllers/Register');
-const loginController = require('./controllers/Login');
-const createPostController = require('./controllers/Post');
+const registerController = require('./controllers/RegisterController');
+const loginController = require('./controllers/LoginController');
+const createPostController = require('./controllers/PostController');
+const uploadMiddleware = multer({ dest: 'uploads/' });
 
 const app = express();
 const whitelist = ['http://localhost:3000'];
@@ -25,7 +28,7 @@ mongoose.connect(process.env.mongoDB);
 // Endpoints
 app.post('/register', registerController);
 app.get('/login', loginController);
-app.post('/post', createPostController);
+app.post('/post', uploadMiddleware.single('file'), createPostController);
 
 app.listen(4000, 'localhost', () => {
   console.log('server started successfully');
