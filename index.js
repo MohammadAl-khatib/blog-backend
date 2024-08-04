91de18e42924c22b75cae008b90b639a45c49666
MohammadAl-khatib <alk20489@gmail.com>
2024-08-04 19:00:06 +0300

    create post endpoint

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const registerController = require('./controllers/Register');
 const loginController = require('./controllers/Login');
+const createPostController = require('./controllers/Post');
 
 const app = express();
 const whitelist = ['http://localhost:3000'];
@@ -16,12 +18,14 @@ app.use(cors({
     if (whitelist.includes(origin)) { return callback(null, true); }
   }
 }));
+app.use(cookieParser());
 
 mongoose.connect(process.env.mongoDB);
 
 // Endpoints
 app.post('/register', registerController);
 app.get('/login', loginController);
+app.post('/post', createPostController);
 
 app.listen(4000, 'localhost', () => {
   console.log('server started successfully');
